8d15663c4b4d193870684ba14055b05d30955b2a
Margus <98420540+marguspollm@users.noreply.github.com>
2026-03-23 20:25:19 +0200

    Chnage result messages

--- src/components/ResultMessage.tsx
+++ src/components/ResultMessage.tsx
@@ -6,12 +6,14 @@ type ResultMessageProps = {
 function ResultMessage({ score, totalQuestions }: ResultMessageProps) {
   const percent = (score / totalQuestions) * 100;
   const setMessage = () => {
-    if (percent === 100) return "Super";
-    if (percent >= 80) return "Väga hea";
-    if (percent >= 50) return "Hea";
-    if (percent >= 20) return "Nigel";
-    if (percent > 0) return "Kahju";
-    return "Proovi uuesti";
+    if (percent === 100) return "Super, tunned Eestimaad läbi ja lõhki!";
+    if (percent >= 80)
+      return "Väga hea, oled kahe kõrva vahele head ja paremat meelde jätnud!";
+    if (percent >= 50) return "Hästi tehtud aga alati saab paremini!";
+    if (percent >= 20)
+      return "Pole hullu, kes see ikka jõuab kõike meelde jätta!";
+    if (percent > 0) return "Tühja sest tulemusest, avastamise rõõm on suurem!";
+    return "Ära pead norgu lase, proovi uuesti!";
   };
   return <section className="results-message">{setMessage()}</section>;
 }
